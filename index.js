134076ca56023151143f88d0e6adf6ef26d616b3
Enrico Digman <essdigman@gmail.com>
2022-10-06 07:49:18 +0800

    Fixed declarations

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const qrcode = require('qrcode-terminal');
-let name = "Enrico Sebastian S. Digman", num = "09477093576", email = "enricosebastian.digman@siteminder.com";
+let name = "Enrico Sebastian S. Digman";
+let num = "09477093576";
+let email = "enricosebastian.digman@siteminder.com";
 
 console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:"SocialD3$K09F";', {small: true}));
-console.log(qrcode.generate(`BEGIN:VCARD VERSION:4.0\nFN:${name}\nTEL;TYPE=cell:${num}\nEMAIL:${email}\nEND:VCARD`, {small: true}));
+console.log(qrcode.generate(`BEGIN:VCARD VERSION:4.0\nFN:${name}\nTEL;TYPE=cell:${num} EMAIL:${email} END:VCARD`, {small: true}));

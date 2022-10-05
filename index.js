7dd9694eecb5b647e8c83636f424f48f7e74d401
Enrico Digman <essdigman@gmail.com>
2022-10-05 20:08:14 +0800

    Optimized code for vard

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const qrcode = require('qrcode-terminal');
+let name = "Enrico Sebastian S. Digman", num = "09477093576", email = "enricosebastian.digman@siteminder.com";
 
-console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:"SocialD3$K09F";'));
-console.log(qrcode.generate('BEGIN:VCARD VERSION:4.0\r\nFN:Enrico Sebastian S. Digman\r\nTEL;TYPE=cell:09477093576\r\nEMAIL:enricosebastian.digman@siteminder.com\r\nEND:VCARD', {small: true}));
+console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:"SocialD3$K09F";', {small: true}));
+console.log(qrcode.generate(`BEGIN:VCARD VERSION:4.0\r\nFN:${name}\r\nTEL;TYPE=cell:${num}\r\nEMAIL:${email}\r\nEND:VCARD`, {small: true}));

45946f3e7c166319605939a8ce91857979925ffc
Enrico Digman <essdigman@gmail.com>
2022-10-06 07:58:13 +0800

    Fixed template literal

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ let num = "09477093576";
 let email = "enricosebastian.digman@siteminder.com";
 
 console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:"SocialD3$K09F";', {small: true}));
-console.log(qrcode.generate(`BEGIN:VCARD VERSION:4.0\nFN:${name}\nTEL;TYPE=cell:${num} EMAIL:${email} END:VCARD`, {small: true}));
+console.log(qrcode.generate(`BEGIN:VCARD VERSION:4.0
+FN:${name}
+TEL;TYPE=cell:${num}
+EMAIL:${email}
+END:VCARD`, {small: true}));

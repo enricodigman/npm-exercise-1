90ba6bd66f6dacb527c9b5622fc37267830d8cb3
Enrico Digman <essdigman@gmail.com>
2022-10-05 19:56:12 +0800

    Generated qr code for vcard

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const qrcode = require('qrcode-terminal');
 
 console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:"SocialD3$K09F";'));
+console.log(qrcode.generate('BEGIN:VCARD VERSION:4.0\r\nFN:Enrico Sebastian S. Digman\r\nTEL;TYPE=cell:09477093576\r\nEMAIL:enricosebastian.digman@siteminder.com\r\nEND:VCARD', {small: true}));

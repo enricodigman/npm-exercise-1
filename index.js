56951e64948958c564da39ed4c8c547676564739
Enrico Digman <essdigman@gmail.com>
2022-10-07 08:20:45 +0800

    Applied single quotations to intiazlied variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const qrcode = require('qrcode-terminal');
-let name = "Enrico Sebastian S. Digman";
-let num = "09477093576";
-let email = "enricosebastian.digman@siteminder.com";
+let name = 'Enrico Sebastian S. Digman';
+let num = '09477093576';
+let email = 'enricosebastian.digman@siteminder.com';
 
 console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:"SocialD3$K09F";', {small: true}));
 console.log(qrcode.generate(`BEGIN:VCARD VERSION:4.0

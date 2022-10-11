64371d00da3612f7963bc095d503a23a4799a649
Enrico Digman <essdigman@gmail.com>
2022-10-11 09:17:56 +0800

    Implemented module

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ let name = 'Enrico Sebastian S. Digman';
 let num = '09477093576';
 let email = 'enricosebastian.digman@siteminder.com';
 
-console.log(qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:"SocialD3$K09F";', {small: true}));
-console.log(qrcode.generate(`BEGIN:VCARD VERSION:4.0
+console.log(
+  qrcode.generate('WIFI:S:SOCIALDESK - 9th Floor;T:WPA;P:"SocialD3$K09F";', {
+    small: true,
+  })
+);
+console.log(
+  qrcode.generate(
+    `BEGIN:VCARD VERSION:4.0
 FN:${name}
 TEL;TYPE=cell:${num}
 EMAIL:${email}
-END:VCARD`, {small: true}));
+END:VCARD`,
+    { small: true }
+  )
+);
